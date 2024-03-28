4010578bea97ea08e45345dfcea2cf6e5b533731
MarcusKyung <Kyungmj@gmail.com>
2024-03-28 11:29:30 -0700

    remote: unused imports

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python3
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 class color:
     PURPLE = '\033[95m'
